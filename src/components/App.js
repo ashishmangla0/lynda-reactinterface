64de5282286b52eb473aec46a3a98bb43f9c4b7f
ashish <ashishmangla1991@gmail.com>
2020-07-18 15:47:09 +0530

    initla commits

---
+++ src/components/App.js
@@ -0,0 +1,74 @@
+import React,{Component} from 'react';
+import '../css/App.css';
+import AddAppoinments from './AddAppointments';
+import SearchAppoinments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+import {without} from 'lodash';
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state ={
+      // myName : 'Ashish mangla',
+      myAppointments :[],
+      formDisplay: false,
+      lastIndex:0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+  }
+
+deleteAppointment (apt){
+  let tempApts =  this.state.myAppointments;
+  tempApts =  without(tempApts,apt);
+
+  this.setState({
+    myAppointments : tempApts
+  })
+}
+
+componentDidMount(){
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(result =>{
+      const apts = result.map(item=>{
+        item.aptId = this.state.lastIndex;
+        this.setState({
+          lastIndex: this.state.lastIndex + 1
+        })
+        return item;
+      })
+      this.setState({
+        myAppointments: apts
+      });
+    })
+   
+
+}
+
+  render(){
+
+   
+    return (
+      <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+           
+              <AddAppoinments formDisplay={this.state.formDisplay}/>
+               <SearchAppoinments/>
+               <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    );
+  }
+}
+export default App;
